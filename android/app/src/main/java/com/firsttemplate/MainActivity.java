7a4aaaf20f9ee1c2ac5ea8804f072fa8039f019e
Michel Henrique Hoekstra <michel.h.hoekstra@gmail.com>
2020-01-11 14:51:59 -0300

    Adjusted the native layer to set the UI into Immersive mode.

--- android/app/src/main/java/com/firsttemplate/MainActivity.java
+++ android/app/src/main/java/com/firsttemplate/MainActivity.java
@@ -1,8 +1,22 @@
 package com.firsttemplate;
 
+import android.view.View;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onStart()
+  {
+    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    super.onStart();
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
